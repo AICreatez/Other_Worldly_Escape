2678c3a43a8d165fabc5bbd9b66b72f76e6e9219
AICreatez <21582023@student.uwl.ac.uk>
2023-03-07 17:41:25 +0000

    Enemy AI In progress
    Map with enemy script in progress

---
+++ OtherWorldlyEscape/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    NavMeshAgent Enemy;
+
+    GameObject Player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemy = GetComponent<NavMeshAgent>();
+        Player=GameObject.FindWithTag("Player"); //enemy fetching the player using the tag
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Enemy.SetDestination(Player.transform.position);
+    }
+}
