e769933fb044f888c81d0d59dcab46ea481fb757
AICreatez <21582023@student.uwl.ac.uk>
2023-04-28 18:00:02 +0100

    Particles added into gameplay
    Particles, Death Barriers, and Game Over Screen

---
+++ OtherWorldlyEscape/Assets/Scripts/GOPrompt.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GOPrompt : MonoBehaviour
+{
+    public GameObject GOPanel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GOPanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            GOPanel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+   
+
+}
